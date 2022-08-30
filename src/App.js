20e005bd5e468e97c616f51f9f929bdf768c2527
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-08-30 14:10:25 +0300

    Login Components created!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Login from "../src/pages/login/Login";
+import Home from "../src/pages/home/Home";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const user = JSON.parse(localStorage.getItem('username'));
+
+
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <div>
+                    <Routes>
+                        <Route path="/" element={user ? <Home/> : <Navigate to="/login"/>}/>
+                        <Route path="/login" element={!user ? <Login/> : <Navigate to="/"/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
