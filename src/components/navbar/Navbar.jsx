356ae797705329ef59edbf531c4fa7bf22277a8d
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-09-02 14:14:31 +0300

    Home page ready!

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,67 @@
-import React from "react";
 
-const Navbar=()=>{
+import { useState } from "react";
+
+const Navbar=({ todos, setTodos, setHide })=>{
+    const unCompleted = todos.filter((check) => check.checked === false);
+
+
+  const [select, setSelect] = useState("selected", "", "");
+
+
+  const clearCompleted = (e) => {
+    setTodos(todos.filter((todo) => todo.checked === false));
+  };
+
+
+  const selectedButton = (e) => {
+    switch (e.target.id) {
+      case "All":
+        setSelect(["selected", "", ""]);
+        setHide("All");
+        break;
+      case "Active":
+        setSelect(["", "selected", ""]);
+        setHide("Active");
+        break;
+      case "Completed":
+        setSelect(["", "", "selected"]);
+        setHide("Completed");
+        break;
+      default:
+    }
+  };
     return(
-        <div>
-            Navbar
-        </div>
+        <footer className="footer">
+      <span className="todo-count">
+        <strong>{unCompleted.length}</strong>
+        {unCompleted.length > 1 ? " items left" : " item left"}
+
+      </span>
+
+
+      <ul className="filters">
+        <li>
+          <a className={select[0]} id="All" onClick={selectedButton}>
+            All
+          </a>
+        </li>
+        <li>
+          <a className={select[1]} id="Active" onClick={selectedButton}>
+            Active
+          </a>
+        </li>
+        <li>
+          <a className={select[2]} id="Completed" onClick={selectedButton}>
+            Completed
+          </a>
+        </li>
+      </ul>
+
+      <text className="clear-completed" onClick={clearCompleted}>
+        Clear completed
+      </text>
+    </footer>
     )
 }
 export default Navbar;
+
